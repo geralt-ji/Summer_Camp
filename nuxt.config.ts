9f58f6c7e26f6d553be978fbca2ed85d7370a7fc
geralt-ji <geralt-ji@users.noreply.github.com>
2024-12-26 11:19:30 +0000

    deploy: 12c9fa524dff0328aaa7891be6c1cd2a54a6352f

--- nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  app: {
-    baseURL: '/Summer_Camp/', // 仓库名称
-    buildAssetsDir: 'assets', // 不要使用_nuxt
-  },
-})
